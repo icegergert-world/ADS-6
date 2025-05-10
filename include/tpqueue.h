a9bcd2a332e445f2a671a03f000d9c73b3dce97c
icegergert-world <andrejkornev26@gmail.com>
2025-05-10 17:43:27 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,63 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct QueueNode {
+        TPQueue item;
+        QueueNode* link;
+
+        explicit QueueNode(const T& val)
+            : item(val), link(nullptr) {}
+    };
+
+    QueueNode* front;
+
+public:
+    PriorityQueue()
+        : front(nullptr) {}
+
+    ~PriorityQueue() {
+        while (front != nullptr) {
+            QueueNode* tmp = front;
+            front = front->link;
+            delete tmp;
+        }
+    }
+
+    void enqueue(const T& elem) {
+        auto* newElem = new QueueNode(elem);
+
+        if (front == nullptr || elem.prior > front->item.prior) {
+            newElem->link = front;
+            front = newElem;
+            return;
+        }
+
+        QueueNode* curr = front;
+        while (curr->link != nullptr &&
+            curr->link->item.prior <= elem.prior) {
+            curr = curr->link;
+        }
+
+        newElem->link = curr->link;
+        curr->link = newElem;
+    }
+
+    T dequeue() {
+        if (isEmpty()) {
+            throw std::runtime_error("Queue is empty");
+        }
+
+        QueueNode* temp = front;
+        T result = front->item;
+        front = front->link;
+        delete temp;
+        return result;
+    }
+
+    bool isEmpty() const noexcept {
+        return front == nullptr;
+    }
 };
 
 struct SYM {
